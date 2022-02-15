fddd9b721d1dfcdb571c7282c519ba770f0cea27
arvin <arvinvazife@protonmail.com>
2022-02-15 20:14:43 +0330

    ResponeHandler class created for answering to user messages

---
+++ src/main/java/ResponseHandler.java
@@ -0,0 +1,23 @@
+import org.telegram.abilitybots.api.sender.MessageSender;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ResponseHandler {
+
+    private final MessageSender sender;
+
+    public ResponseHandler(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void replyToStart(long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(Constants.START_REPLY);
+        try {
+            sender.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
