4bfcb68de18b1eb070f479e931071ebf116214a0
arvin <arvinvazife@protonmail.com>
2022-02-15 19:40:46 +0330

    Constant class created for constant values

---
+++ src/main/java/Constant.java
@@ -0,0 +1,9 @@
+class Constants {
+    public static final int CREATOR_ID = ***REMOVED***;
+    public static final String TOKEN = "***REMOVED***";
+    public static final String USERNAME = "***REMOVED***";
+
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+}
